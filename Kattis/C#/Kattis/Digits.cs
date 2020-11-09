b54b786f9b2045fce5510b5214bec53b3ee2f4a0
valentk777 <valentk777@gmail.com>
2020-11-09 20:34:24 +0200

    [Kattis] Digits

---
+++ Kattis/C#/Kattis/Digits.cs
@@ -0,0 +1,32 @@
+﻿// #-----------------------------------------------------------
+// # 
+// # https://open.kattis.com/problems/digits
+// # 
+// #-----------------------------------------------------------
+
+using System;
+
+namespace Kattis
+{
+    public class Digits
+    {
+        static void Solve()
+        {
+            var sk0 = Console.ReadLine();
+            while (sk0 != "END")
+            {
+                var sk1 = sk0.Length.ToString();
+                var i = 1;
+                while (sk0 != sk1)
+                {
+                    sk0 = sk1;
+                    sk1 = sk1.Length.ToString();
+                    i++;
+                }
+
+                Console.WriteLine(i);
+                sk0 = Console.ReadLine();
+            }
+        }
+    }
+}
--- Kattis/ProblemsList.md
+++ Kattis/ProblemsList.md
@@ -2,3 +2,4 @@
 
 * [Coin Stacks](https://ncpc20.kattis.com/problems/coinstacks)
 * [Avion](https://open.kattis.com/problems/avion)
+* [Digits](https://open.kattis.com/problems/digits)
