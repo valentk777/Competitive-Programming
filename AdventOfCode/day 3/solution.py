59b9cfa89175c9e04260ef2b0fe7354c06626697
valentk777 <valentk777@gmail.com>
2020-12-03 08:13:05 +0200

    [AdventOfCode] Day 3

--- AdventOfCode/day 3/solution.py
+++ AdventOfCode/day 3/solution.py
@@ -1,15 +1,34 @@
 from utils import read
 
 input_data = read()
-print(input_data)
 
 
-def solve_1():
-    pass
+def get_tree_count_for_slope(right: int, down: int) -> int:
+    line_max = len(input_data)
+    column_max = len(input_data[0])
+    line = 0
+    column = 0
+    count_tree = 0
+    while line < line_max:
+        if input_data[line][column] == '#':
+            count_tree += 1
+        line += down
+        column = (column + right) % column_max
+    return count_tree
 
 
-def solve_2():
-    pass
+def solve_1() -> None:
+    result = get_tree_count_for_slope(3, 1)
+    print(result)
+
+
+def solve_2() -> None:
+    result = (get_tree_count_for_slope(1, 1)
+              * get_tree_count_for_slope(3, 1)
+              * get_tree_count_for_slope(5, 1)
+              * get_tree_count_for_slope(7, 1)
+              * get_tree_count_for_slope(1, 2))
+    print(result)
 
 
 solve_1()
