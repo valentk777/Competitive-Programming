5aa4fe17f5bf4f3865b65db18f0ccc7599c09ab7
valentk777 <valentk777@gmail.com>
2020-12-09 09:19:09 +0200

    [AdventOfCode] Day 9

--- AdventOfCode/2020/day 9/solution.py
+++ AdventOfCode/2020/day 9/solution.py
@@ -1,16 +1,33 @@
+from itertools import combinations
+from typing import List
+
 from utils import read_lines
 
 input_data = read_lines()
+input_data = list(map(int, input_data))
 print(input_data)
 
 
-def solve_1():
-    pass
+def make_sums(data: List[int]):
+    return map(sum, combinations(data, 2))
 
 
-def solve_2():
-    pass
+def solve_1(data: List[int]):
+    n = 25
+    for i in range(len(data) - n):
+        sums = make_sums(data[i:i + n])
+        if data[i + n] not in sums:
+            print(data[i + n])
+            return data[i + n]
 
 
-solve_1()
-solve_2()
+def solve_2(data: List[int], search_number):
+    for i in range(len(data) - 3):
+        for j in range(i + 3, len(data)):
+            if search_number == sum(data[i:j]):
+                print(max(data[i:j]) + min(data[i:j]))
+                break
+
+
+invalid_number = solve_1(input_data)
+solve_2(input_data, invalid_number)
