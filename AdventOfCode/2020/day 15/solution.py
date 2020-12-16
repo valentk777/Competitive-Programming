ed4920d5418b33d733be143244a04fcf46bf332c
valentk777 <valentk777@gmail.com>
2020-12-16 14:04:23 +0200

    [AdventOfCode] Day 15

--- AdventOfCode/2020/day 15/solution.py
+++ AdventOfCode/2020/day 15/solution.py
@@ -1,16 +1,39 @@
+from typing import List
+
 from utils import read_lines
 
 input_data = read_lines()
-print(input_data)
 
 
-def solve_1():
-    pass
+def find_solution(data: List[int], number_of_iterations: int) -> int:
+    n = len(data)
+    d = {data[number - 1]: number for number in range(1, n)}
+
+    current = data[-1]
+    count = n
+    while count != number_of_iterations:
+        if current not in d.keys():
+            d[current] = count
+            current = 0
+        else:
+            new_current = count - d[current]
+            d[current] = count
+            current = new_current
+        count += 1
+    return current
 
 
-def solve_2():
-    pass
+def solve_1(data: List[int]):
+    result = find_solution(data, 2020)
+    print(result)
 
 
-solve_1()
-solve_2()
+def solve_2(data: List[int]):
+    result = find_solution(data, 30000000)
+    print(result)
+
+
+input_data = list(map(int, input_data[0].split(',')))
+
+solve_1(input_data)
+solve_2(input_data)
