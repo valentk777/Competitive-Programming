5c64b42a73bd4427cc58c5bd163c1f3ac527e7a6
valentk777 <valentk777@gmail.com>
2020-12-18 09:25:15 +0200

    [AdventOfCode] Day 18

--- AdventOfCode/2020/day 18/solution.py
+++ AdventOfCode/2020/day 18/solution.py
@@ -1,16 +1,72 @@
+from typing import List
+
 from utils import read_lines
 
 input_data = read_lines()
-print(input_data)
+input_data = list(map(lambda x: x.replace(" ", ""), input_data))
 
 
-def solve_1():
-    pass
+def evaluate_equal_expression(expression: str):
+    if "*" not in expression:
+        return str(eval(expression))
+
+    if "+" not in expression:
+        return str(eval(expression))
+
+    number = ""
+    first = True
+    for i in range(len(expression)):
+        if expression[i] in ["+", "-", "*", "/"]:
+            if first:
+                first = False
+            else:
+                number = str(eval(number))
+
+        number += expression[i]
+
+    return str(eval(number))
 
 
-def solve_2():
-    pass
+def evaluate_not_equal_expression(expression: str):
+    if "*" not in expression:
+        return str(eval(expression))
+
+    if "+" not in expression:
+        return str(eval(expression))
+
+    number = 1
+    for i in map(eval, expression.split("*")):
+        number *= i
+
+    return str(number)
 
 
-solve_1()
-solve_2()
+def evaluate(evaluation_func):
+    def step(expression):
+        expression_without_brackets = ""
+        for e in expression:
+            if e == ")":
+                idx = expression_without_brackets.rfind("(")
+                result = evaluation_func(expression_without_brackets[idx + 1:])
+                expression_without_brackets = expression_without_brackets[:idx] + result
+            else:
+                expression_without_brackets += e
+        return int(evaluation_func(expression_without_brackets))
+
+    return step
+
+
+def get_sum_of_expressions(data: List[str], evaluation_func):
+    return sum(map(evaluate(evaluation_func), data))
+
+
+def solve_1(data: List[str]) -> None:
+    print(get_sum_of_expressions(data, evaluate_equal_expression))
+
+
+def solve_2(data: List[str]) -> None:
+    print(get_sum_of_expressions(data, evaluate_not_equal_expression))
+
+
+solve_1(input_data)
+solve_2(input_data)
