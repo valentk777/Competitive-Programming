367223a915ed39780d66f0826ae3a050374726ee
Valentinas <31697821+valentk777@users.noreply.github.com>
2022-09-15 08:06:32 +0300

    [Codeforces] Submit few solutions

---
+++ Codeforces/Python/Educational Codeforces Round 101/B.py
@@ -0,0 +1,36 @@
+# -----------------------------------------------------------
+# URL    : https://codeforces.com/contest/1469/problem/B
+# Title  : Red and Blue
+# Notes  : tag-codeforces, tag-problem-B, tag-div-2
+# -----------------------------------------------------------
+
+
+def solve():
+    n = int(input())
+    r = list(map(int, input().split()))
+    m = int(input())
+    b = list(map(int, input().split()))
+
+    # max value of function f
+    dp = [[-10 ** 9 for _ in range(m + 1)] for _ in range(n + 1)]
+    dp[0][0] = 0
+    ans = 0
+
+    for i in range(n + 1):
+        for j in range(m + 1):
+            if i < n:
+                dp[i + 1][j] = max(dp[i + 1][j], dp[i][j] + r[i])
+
+            if j < m:
+                dp[i][j + 1] = max(dp[i][j + 1], dp[i][j] + b[j])
+
+            ans = max(ans, dp[i][j])
+
+    return ans
+
+
+if __name__ == "__main__":
+    t = int(input())
+
+    for _ in range(t):
+        print(solve())
---
+++ Codeforces/Python/Round #447 (Div. 2)/A.py
@@ -0,0 +1,24 @@
+# -----------------------------------------------------------
+# URL    : https://codeforces.com/contest/894/problem/A
+# Title  : QAQ
+# Notes  : tag-codeforces, tag-problem-A, tag-div-2
+# -----------------------------------------------------------
+
+
+def solve_slow():
+    n = input()
+    nn = list(filter(lambda l: l == "Q" or l == "A", list(n)))
+    n_len = len(nn)
+    _sum = 0
+
+    for i in range(n_len - 2):
+        for j in range(i + 1, n_len - 1):
+            for k in range(j + 1, n_len):
+                if (nn[i] == 'Q') and (nn[j] == 'A') and (nn[k] == 'Q'):
+                    _sum += 1
+
+    return _sum
+
+
+if __name__ == "__main__":
+    print(solve_slow())
