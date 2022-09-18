102b23ad4f3879262329466b14d632bc03cafc2b
Valentinas <31697821+valentk777@users.noreply.github.com>
2022-09-18 09:07:52 +0300

    [Codeforces] Add few solutions

---
+++ Codeforces/Python/Round #725 (Div. 3)/A.py
@@ -0,0 +1,37 @@
+# -----------------------------------------------------------
+# URL    : https://codeforces.com/contest/1538/problem/A
+# Title  : Stone Game
+# Notes  : tag-codeforces, tag-problem-A, tag-div-3
+# -----------------------------------------------------------
+
+
+def solve():
+    n = int(input())
+    a = list(map(int, input().split()))
+
+    _min = 10 ** 5
+    _min_pos = -1
+    _max = -10 ** 5
+    _max_pos = -1
+
+    for i in range(n):
+        if a[i] < _min:
+            _min = a[i]
+            _min_pos = i
+
+        if a[i] > _max:
+            _max = a[i]
+            _max_pos = i
+
+    x = min(_min_pos, _max_pos)
+    y = max(_min_pos, _max_pos)
+
+    # min (min(<from beginning till max position> , <all minus few from beginning>), <from beginning + from right>)
+    return min(min(y + 1, n - x), x + n - y + 1)
+
+
+if __name__ == "__main__":
+    t = int(input())
+
+    for _ in range(t):
+        print(solve())
---
+++ Codeforces/Python/Round #727 (Div. 2)/B.py
@@ -0,0 +1,27 @@
+# -----------------------------------------------------------
+# URL    : https://codeforces.com/contest/1539/problem/B
+# Title  : Love Song
+# Notes  : tag-codeforces, tag-problem-B, tag-div-2
+# -----------------------------------------------------------
+
+
+MODULE_N = 10 ** 9 + 7
+
+
+def solve():
+    n, q = list(map(int, input().split()))
+    s = input()
+
+    sums = [0 for _ in range(n + 1)]
+    sums[0] = 0
+
+    for i in range(1, n + 1):
+        sums[i] = sums[i - 1] + ord(s[i - 1]) - ord('a') + 1
+
+    for _ in range(q):
+        l, r = list(map(int, input().split()))
+        print(sums[r] - sums[l - 1])
+
+
+if __name__ == "__main__":
+    solve()
