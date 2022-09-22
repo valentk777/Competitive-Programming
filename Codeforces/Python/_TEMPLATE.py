62d85b0777a9d729d0e696c53490d842ad393948
Valentinas <31697821+valentk777@users.noreply.github.com>
2022-09-22 09:27:31 +0300

    [Fix] add template and input file

---
+++ Codeforces/Python/_TEMPLATE.py
@@ -0,0 +1,41 @@
+# -----------------------------------------------------------
+# URL    : https://codeforces.com/contest/1451/problem/C
+# Title  : String Equality
+# Notes  : tag-codeforces, tag-problem-C, tag-div-2
+# -----------------------------------------------------------
+
+# ---------------------------------------------------Shared part--------------------------------------------------------
+import os
+import time
+from sys import stdin, maxsize
+
+inp = lambda: stdin.readline().strip()
+iinp = lambda: int(inp())
+intl = lambda: list(map(int, inp().split()))
+strl = lambda: list(inp().split())
+
+MOD = 10 ** 9 + 7
+INF = maxsize
+
+
+# -------------------------------------------------------Solution-------------------------------------------------------
+
+def solve():
+    pass
+
+
+def run():
+    t = iinp()
+
+    for _ in range(t):
+        print(solve())
+
+
+if __name__ == "__main__":
+    if os.environ.get("DEBUG_CODEFORCES"):
+        stdin = open('../input.txt', 'r')
+        start_time = time.time()
+        run()
+        print("\n--- %s seconds ---\n" % (time.time() - start_time))
+    else:
+        run()
