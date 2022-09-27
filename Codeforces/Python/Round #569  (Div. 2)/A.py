1bdb3a229d455725287445bf6ab1943266ec47a9
Valentinas <31697821+valentk777@users.noreply.github.com>
2022-09-27 10:44:53 +0300

    [Codeforces] Add few solutions

---
+++ Codeforces/Python/Round #569  (Div. 2)/A.py
@@ -0,0 +1,46 @@
+# -----------------------------------------------------------
+# URL    : https://codeforces.com/contest/1180/problem/A
+# Title  : Alex and a Rhombus
+# Notes  : tag-codeforces, tag-problem-A, tag-div-2
+# -----------------------------------------------------------
+
+# ---------------------------------------------------Shared part--------------------------------------------------------
+import os
+import time
+from sys import stdin, maxsize
+
+inp = lambda: stdin.readline().strip()
+iinp = lambda: int(inp())
+intl = lambda: list(map(int, inp().split()))
+strl = lambda: list(inp().split())
+
+MOD = 10 ** 9 + 7
+INF = maxsize
+
+
+# -------------------------------------------------------Solution-------------------------------------------------------
+
+def solve():
+    n = iinp()
+
+    dp = [-1 for _ in range(n + 1)]
+    dp[0] = 1
+
+    for i in range(1, n + 1):
+        dp[i] = dp[i - 1] + 4 * i
+
+    return dp[n - 1]
+
+
+def run():
+    print(solve())
+
+
+if __name__ == "__main__":
+    if os.environ.get("DEBUG_CODEFORCES"):
+        stdin = open("../input.txt", "r")
+        start_time = time.time()
+        run()
+        print("\n--- %s seconds ---\n" % (time.time() - start_time))
+    else:
+        run()
---
+++ Codeforces/Python/Round #794  (Div. 2)/B.py
@@ -0,0 +1,58 @@
+# -----------------------------------------------------------
+# URL    : https://codeforces.com/contest/1686/problem/B
+# Title  : Odd Subarrays
+# Notes  : tag-codeforces, tag-problem-B, tag-div-2
+# -----------------------------------------------------------
+
+# ---------------------------------------------------Shared part--------------------------------------------------------
+import os
+import time
+from sys import stdin, maxsize
+
+inp = lambda: stdin.readline().strip()
+iinp = lambda: int(inp())
+intl = lambda: list(map(int, inp().split()))
+strl = lambda: list(inp().split())
+
+MOD = 10 ** 9 + 7
+INF = maxsize
+
+
+# -------------------------------------------------------Solution-------------------------------------------------------
+
+def solve():
+    n = iinp()
+    p = intl()
+
+    # max number of splits
+    dp = [-1 for _ in range(n + 1)]
+    dp[0] = 0
+
+    i = 1
+
+    while i < n:
+        if p[i - 1] > p[i]:
+            dp[i] = dp[i - 1] + 1
+            i += 1
+
+        dp[i] = dp[i - 1]
+        i += 1
+
+    return dp[n - 1]
+
+
+def run():
+    t = iinp()
+
+    for _ in range(t):
+        print(solve())
+
+
+if __name__ == "__main__":
+    if os.environ.get("DEBUG_CODEFORCES"):
+        stdin = open("../input.txt", "r")
+        start_time = time.time()
+        run()
+        print("\n--- %s seconds ---\n" % (time.time() - start_time))
+    else:
+        run()
