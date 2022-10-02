c2fb4fd14b524f4a39b1149176e12ecb1e2b082b
Valentinas <31697821+valentk777@users.noreply.github.com>
2022-10-02 10:35:54 +0300

    [Codeforces] Add few solutions

---
+++ Codeforces/Python/Round #215 (Div. 2)/B.py
@@ -0,0 +1,57 @@
+# -----------------------------------------------------------
+# URL    : https://codeforces.com/contest/368/problem/B
+# Title  : Sereja and Suffixes
+# Notes  : tag-codeforces, tag-problem-B, tag-div-2
+# -----------------------------------------------------------
+
+# ---------------------------------------------------Shared part--------------------------------------------------------
+import os
+import time
+from collections import defaultdict
+from sys import stdin, maxsize
+
+inp = lambda: stdin.readline().strip()
+iinp = lambda: int(inp())
+intl = lambda: list(map(int, inp().split()))
+strl = lambda: list(inp().split())
+list_to_string = lambda _a: " ".join(map(str, _a))
+_dp = lambda default_value: defaultdict(lambda: default_value)
+
+MOD = 10 ** 9 + 7
+INF = maxsize
+
+
+# -------------------------------------------------------Solution-------------------------------------------------------
+
+def solve():
+    n, m = intl()
+    a = intl()
+
+    dp = _dp(0)
+    find = _dp(0)
+    dp[n] = 0
+
+    for i in range(n - 1, -1, -1):
+        if find[a[i]] == 0:
+            dp[i] = dp[i + 1] + 1
+            find[a[i]] += 1
+        else:
+            dp[i] = dp[i + 1]
+
+    for i in range(m):
+        l = iinp()
+        print(dp[l - 1])
+
+
+def run():
+    solve()
+
+
+if __name__ == "__main__":
+    if os.environ.get("DEBUG_CODEFORCES"):
+        stdin = open("../input.txt", "r")
+        start_time = time.time()
+        run()
+        print("\n--- %s seconds ---\n" % (time.time() - start_time))
+    else:
+        run()
---
+++ Codeforces/Python/Round #697 (Div. 3)/B.py
@@ -0,0 +1,56 @@
+# -----------------------------------------------------------
+# URL    : https://codeforces.com/contest/1475/problem/B
+# Title  : New Year's Number
+# Notes  : tag-codeforces, tag-problem-B, tag-div-2
+# -----------------------------------------------------------
+
+# ---------------------------------------------------Shared part--------------------------------------------------------
+import os
+import time
+from collections import defaultdict
+from sys import stdin, maxsize
+
+inp = lambda: stdin.readline().strip()
+iinp = lambda: int(inp())
+intl = lambda: list(map(int, inp().split()))
+strl = lambda: list(inp().split())
+list_to_string = lambda _a: " ".join(map(str, _a))
+_dp = lambda default_value: defaultdict(lambda: default_value)
+
+MOD = 10 ** 9 + 7
+INF = maxsize
+
+
+# -------------------------------------------------------Solution-------------------------------------------------------
+
+def solve():
+    t = iinp()
+
+    dp = _dp(0)
+    dp[2020] = 1
+    dp[2021] = 1
+
+    for i in range(2022, 1000001):
+        dp[i] = dp[i - 2020] | dp[i - 2021]
+
+    for _ in range(t):
+        n = iinp()
+
+        if dp[n] == 1:
+            print("YES")
+        else:
+            print("NO")
+
+
+def run():
+    solve()
+
+
+if __name__ == "__main__":
+    if os.environ.get("DEBUG_CODEFORCES"):
+        stdin = open("../input.txt", "r")
+        start_time = time.time()
+        run()
+        print("\n--- %s seconds ---\n" % (time.time() - start_time))
+    else:
+        run()
