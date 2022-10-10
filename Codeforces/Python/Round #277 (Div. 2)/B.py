b4275f8620d2a9041f4b92a7ef0853b3c9e7fabd
Valentinas <31697821+valentk777@users.noreply.github.com>
2022-10-10 17:05:19 +0300

    [Codeforces] Add few solutions

---
+++ Codeforces/Python/Round #277 (Div. 2)/B.py
@@ -0,0 +1,117 @@
+# -----------------------------------------------------------
+# URL    : https://codeforces.com/contest/486/problem/B
+# Title  : OR in Matrix
+# Notes  : tag-codeforces, tag-problem-B, tag-div-2
+# -----------------------------------------------------------
+
+# ---------------------------------------------------Shared part--------------------------------------------------------
+import os
+import time
+from collections import defaultdict
+from sys import stdin, maxsize
+
+inp = lambda: stdin.readline().strip()
+iinp = lambda: int(inp())
+intl = lambda: list(map(int, inp().split()))
+strl = lambda: list(inp().split())
+list_to_string = lambda _a: "".join(map(str, _a))
+list_to_string_list = lambda _a: " ".join(map(str, _a))
+_dp = lambda default_value: defaultdict(lambda: default_value)
+print_dp = lambda _dict: list(map(lambda item: print(f"{item[0]} = {item[1]}"), _dict.items()))
+
+MOD = 10 ** 9 + 7
+INF = maxsize
+
+
+# -------------------------------------------------------Solution-------------------------------------------------------
+
+def solve():
+    m, n = intl()
+    matrix = []
+
+    for i in range(m):
+        s = intl()
+        matrix.append(s)
+
+    m_sums = [0 for _ in range(m)]
+    n_sums = [0 for _ in range(n)]
+
+    for i in range(m):
+        _sum = sum(matrix[i])
+        m_sums[i] = _sum
+
+    for j in range(n):
+        _sum = 0
+
+        for i in range(m):
+            _sum += matrix[i][j]
+
+        n_sums[j] = _sum
+
+    if n not in m_sums and sum(m_sums) != 0:
+        print("NO")
+        return
+
+    if m not in n_sums and sum(n_sums) != 0:
+        print("NO")
+        return
+
+    ats = []
+
+    for i in range(m):
+        ats.append([0 for _ in range(n)])
+
+    for i in range(m):
+        if m_sums[i] == n:
+            ats[i] = [-1 for i in range(n)]
+
+    for i in range(n):
+        for j in range(m):
+            if ats[j][i] == -1:
+                if n_sums[i] == m:
+                    ats[j][i] = 1
+                else:
+                    ats[j][i] = 0
+
+    ats_m_sums = [0 for _ in range(m)]
+    ats_n_sums = [0 for _ in range(n)]
+
+    for i in range(m):
+        _sum = 0
+        for j in range(n):
+            _sum |= ats[i][j]
+
+        ats_m_sums[i] = _sum
+
+    for j in range(n):
+        _sum = 0
+
+        for i in range(m):
+            _sum |= ats[i][j]
+
+        ats_n_sums[j] = _sum
+
+    for i in range(m):
+        for j in range(n):
+            if (ats_n_sums[j] | ats_m_sums[i]) != matrix[i][j]:
+                print("NO")
+                return
+
+    print("YES")
+
+    for i in range(m):
+        print(list_to_string_list(ats[i]))
+
+
+def run():
+    solve()
+
+
+if __name__ == "__main__":
+    if os.environ.get("DEBUG_CODEFORCES"):
+        stdin = open("../input.txt", "r")
+        start_time = time.time()
+        run()
+        print("\n--- %s seconds ---\n" % (time.time() - start_time))
+    else:
+        run()
---
+++ Codeforces/Python/Round #297 (Div. 2)/A.py
@@ -0,0 +1,57 @@
+# -----------------------------------------------------------
+# URL    : https://codeforces.com/contest/525/problem/A
+# Title  : Vitaliy and Pie
+# Notes  : tag-codeforces, tag-problem-A, tag-div-2
+# -----------------------------------------------------------
+
+# ---------------------------------------------------Shared part--------------------------------------------------------
+import os
+import time
+from collections import defaultdict
+from sys import stdin, maxsize
+
+inp = lambda: stdin.readline().strip()
+iinp = lambda: int(inp())
+intl = lambda: list(map(int, inp().split()))
+strl = lambda: list(inp().split())
+list_to_string = lambda _a: "".join(map(str, _a))
+list_to_string_list = lambda _a: " ".join(map(str, _a))
+_dp = lambda default_value: defaultdict(lambda: default_value)
+print_dp = lambda _dict: list(map(lambda item: print(f"{item[0]} = {item[1]}"), _dict.items()))
+
+MOD = 10 ** 9 + 7
+INF = maxsize
+
+
+# -------------------------------------------------------Solution-------------------------------------------------------
+
+def solve():
+    n = iinp()
+    s = inp()
+    _keys = _dp(0)
+    _missing = 0
+
+    for i in range(n * 2 - 2):
+        if i & 1 == 0:
+            _keys[s[i]] += 1
+        else:
+            if _keys[s[i].lower()] == 0:
+                _missing += 1
+            else:
+                _keys[s[i].lower()] -= 1
+
+    return _missing
+
+
+def run():
+    print(solve())
+
+
+if __name__ == "__main__":
+    if os.environ.get("DEBUG_CODEFORCES"):
+        stdin = open("../input.txt", "r")
+        start_time = time.time()
+        run()
+        print("\n--- %s seconds ---\n" % (time.time() - start_time))
+    else:
+        run()
