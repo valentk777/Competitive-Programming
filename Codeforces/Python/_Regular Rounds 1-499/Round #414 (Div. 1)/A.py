530317b2f82607bb4c897aa6b18dc2dd1bda290c
Valentinas <31697821+valentk777@users.noreply.github.com>
2022-11-02 12:13:31 +0200

    [Codeforces] Add few solutions

---
+++ Codeforces/Python/_Regular Rounds 1-499/Round #414 (Div. 1)/A.py
@@ -0,0 +1,63 @@
+# -----------------------------------------------------------
+# URL    : https://codeforces.com/contest/794/problem/A
+# Title  : A. Bank Robbery
+# Notes  : tag-codeforces, tag-problem-A, tag-div-1
+# -----------------------------------------------------------
+
+# ---------------------------------------------------Shared part--------------------------------------------------------
+import os
+import time
+from collections import defaultdict, Counter
+from sys import stdin, maxsize, stdout
+
+inp = lambda: stdin.readline().strip()
+iinp = lambda: int(inp())
+intl = lambda: list(map(int, inp().split()))
+strl = lambda: list(inp().split())
+list_to_string = lambda _a: "".join(map(str, _a))
+list_to_string_list = lambda _a: " ".join(map(str, _a))
+_dp = lambda default_value: defaultdict(lambda: default_value)
+flush = lambda: stdout.flush()
+print_flush = lambda _text: (print(_text), flush())
+
+
+def print_dp(_dict):
+    for item in _dict.items():
+        print(f"{item[0]} = {item[1]}")
+
+
+MOD = 10 ** 9 + 7
+INF = maxsize
+A = 911382323
+M = 9999999999879998
+
+
+# -------------------------------------------------------Solution-------------------------------------------------------
+
+def solve():
+    a, b, c = intl()
+    n = iinp()
+    x = intl()
+
+    _count = Counter(x)
+    ans = 0
+
+    for key, value in _count.items():
+        if b < key < c:
+            ans += value
+
+    return ans
+
+
+def run():
+    print(solve())
+
+
+if __name__ == "__main__":
+    if os.environ.get("DEBUG_CODEFORCES"):
+        stdin = open("../../input.txt", "r")
+        start_time = time.time()
+        run()
+        print("\n--- %s seconds ---\n" % (time.time() - start_time))
+    else:
+        run()
---
+++ Codeforces/Python/_Regular Rounds 1-499/Round #70 (Div. 2)/A.py
@@ -0,0 +1,75 @@
+# -----------------------------------------------------------
+# URL    : https://codeforces.com/contest/78/problem/A
+# Title  : A. Haiku
+# Notes  : tag-codeforces, tag-problem-A, tag-div-2
+# -----------------------------------------------------------
+
+# ---------------------------------------------------Shared part--------------------------------------------------------
+import os
+import time
+from collections import defaultdict
+from sys import stdin, maxsize, stdout
+
+inp = lambda: stdin.readline().strip()
+iinp = lambda: int(inp())
+intl = lambda: list(map(int, inp().split()))
+strl = lambda: list(inp().split())
+list_to_string = lambda _a: "".join(map(str, _a))
+list_to_string_list = lambda _a: " ".join(map(str, _a))
+_dp = lambda default_value: defaultdict(lambda: default_value)
+flush = lambda: stdout.flush()
+print_flush = lambda _text: (print(_text), flush())
+
+
+def print_dp(_dict):
+    for item in _dict.items():
+        print(f"{item[0]} = {item[1]}")
+
+
+MOD = 10 ** 9 + 7
+INF = maxsize
+A = 911382323
+M = 9999999999879998
+
+
+# -------------------------------------------------------Solution-------------------------------------------------------
+
+def solve():
+    text = []
+
+    for i in range(100):
+        try:
+            s = inp()
+            text.append(s)
+        except:
+            break
+
+    text = list(filter(lambda x: x != "", text))
+
+    if len(text) != 3:
+        return "NO"
+
+    if text[0].count("a") + text[0].count("e") + text[0].count("i") + text[0].count("o") + text[0].count("u") != 5:
+        return "NO"
+
+    if text[1].count("a") + text[1].count("e") + text[1].count("i") + text[1].count("o") + text[1].count("u") != 7:
+        return "NO"
+
+    if text[2].count("a") + text[2].count("e") + text[2].count("i") + text[2].count("o") + text[2].count("u") != 5:
+        return "NO"
+
+    return "YES"
+
+
+def run():
+    print(solve())
+
+
+if __name__ == "__main__":
+    if os.environ.get("DEBUG_CODEFORCES"):
+        stdin = open("../../input.txt", "r")
+        start_time = time.time()
+        run()
+        print("\n--- %s seconds ---\n" % (time.time() - start_time))
+    else:
+        run()
