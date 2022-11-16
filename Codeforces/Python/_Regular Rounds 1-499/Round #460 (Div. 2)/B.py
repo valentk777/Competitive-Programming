e940e1cb02848b3b678b483098eb9b210b31d8f7
Valentinas <31697821+valentk777@users.noreply.github.com>
2022-11-16 08:42:48 +0200

    [Codeforces] Add few solutions

---
+++ Codeforces/Python/_Regular Rounds 1-499/Round #460 (Div. 2)/B.py
@@ -0,0 +1,78 @@
+# -----------------------------------------------------------
+# URL    : https://codeforces.com/contest/919/problem/B
+# Title  : B. Perfect Number
+# Notes  : tag-codeforces, tag-problem-B, tag-div-2
+# -----------------------------------------------------------
+
+# ---------------------------------------------------Shared part--------------------------------------------------------
+import math
+import os
+import time
+from collections import defaultdict, Counter
+from sys import stdin, maxsize, stdout
+
+inp = lambda: stdin.readline().strip()
+iinp = lambda: int(inp())
+intl = lambda: list(map(int, inp().split()))
+strl = lambda: list(inp().split())
+list_to_string = lambda _a: "".join(map(str, _a))
+list_to_string_list = lambda _a: " ".join(map(str, _a))
+_dp = lambda default_value: defaultdict(lambda: default_value)
+flush = lambda: stdout.flush()
+print_flush = lambda _text: (print(_text), flush())
+fact = lambda number: math.factorial(number)
+cnt = lambda _a: Counter(_a)
+
+
+def lcm(a, b):
+    return a * b // math.gcd(a, b)
+
+
+def print_dp(_dict):
+    for item in _dict.items():
+        print(f"{item[0]} = {item[1]}")
+
+
+MOD = 10 ** 9 + 7
+INF = maxsize
+A = 911382323
+M = 9999999999879998
+
+
+# -------------------------------------------------------Solution-------------------------------------------------------
+def get_numbers():
+    numbers = set()
+
+    for a in range(10):
+        for b in range(10):
+            for c in range(10):
+                for d in range(10):
+                    for e in range(10):
+                        for f in range(10):
+                            for g in range(10):
+                                for h in range(10):
+                                    if a + b + c + d + e + f + g + h == 10:
+                                        numbers.add(int(f"{a}{b}{c}{d}{e}{f}{g}{h}"))
+
+                    if len(numbers) > 10000:
+                        return sorted(numbers)
+
+
+def solve():
+    k = iinp()
+    numbers = get_numbers()
+    return numbers[k - 1]
+
+
+def run():
+    print(solve())
+
+
+if __name__ == "__main__":
+    if os.environ.get("DEBUG_CODEFORCES"):
+        stdin = open("../../input.txt", "r")
+        start_time = time.time()
+        run()
+        print("\n--- %s seconds ---\n" % (time.time() - start_time))
+    else:
+        run()
