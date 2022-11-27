cd6d6c862314eaac9743cfd5c5972f977c03c325
Valentinas <31697821+valentk777@users.noreply.github.com>
2022-11-27 13:00:07 +0200

    [Codeforces] Add few solutions

---
+++ Codeforces/Python/_Global Rounds/Codeforces Global Round 24/A.py
@@ -0,0 +1,65 @@
+# ---------------------------------------------------------------------------------------
+# URL    : https://codeforces.com/contest/1764/problem/A
+# Title  : Doremy's Paint
+# Tags   : tag-codeforces, tag-problem-A, tag-difficulty-0
+# Notes  : greedy
+# ---------------------------------------------------------------------------------------
+
+# ---------------------------------------------------Shared part--------------------------------------------------------
+import math
+import os
+import time
+from collections import defaultdict, Counter
+from sys import stdin, maxsize, stdout
+
+inp = lambda: stdin.readline().strip()
+iinp = lambda: int(inp())
+intl = lambda: list(map(int, inp().split()))
+strl = lambda: list(inp().split())
+list_to_string = lambda _a: "".join(map(str, _a))
+list_to_string_list = lambda _a: " ".join(map(str, _a))
+_dp = lambda default_value: defaultdict(lambda: default_value)
+flush = lambda: stdout.flush()
+print_flush = lambda _text: (print(_text), flush())
+fact = lambda number: math.factorial(number)
+cnt = lambda _a: Counter(_a)
+
+
+def lcm(a, b):
+    return a * b // math.gcd(a, b)
+
+
+def print_dp(_dict):
+    for item in _dict.items():
+        print(f"{item[0]} = {item[1]}")
+
+
+MOD = 10 ** 9 + 7
+INF = maxsize
+A = 911382323
+M = 9999999999879998
+
+
+# -------------------------------------------------------Solution-------------------------------------------------------
+def solve():
+    n = iinp()
+    a = intl()
+
+    return 1, n
+
+
+def run():
+    t = iinp()
+
+    for _ in range(t):
+        print(*solve())
+
+
+if __name__ == "__main__":
+    if os.environ.get("DEBUG_CODEFORCES"):
+        stdin = open("../../input.txt", "r")
+        start_time = time.time()
+        run()
+        print("\n--- %s seconds ---\n" % (time.time() - start_time))
+    else:
+        run()
