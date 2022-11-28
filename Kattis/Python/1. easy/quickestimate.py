d5b6ffcd05a72adaf802d7ce09226ca749930e5e
Valentinas <31697821+valentk777@users.noreply.github.com>
2022-11-28 10:37:50 +0200

    [Kattis] Add few solutions

---
+++ Kattis/Python/1. easy/quickestimate.py
@@ -0,0 +1,50 @@
+# -----------------------------------------------------------
+# URL    : https://open.kattis.com/problems/quickestimate
+# Title  : Quick Estimates
+# Notes  : tag-kattis, tag-easy
+# -----------------------------------------------------------
+
+# ---------------------------------------------------Shared part--------------------------------------------------------
+import os
+import time
+from collections import defaultdict
+from sys import stdin, maxsize
+
+inp = lambda: stdin.readline().strip("\n")
+iinp = lambda: int(inp())
+intl = lambda: list(map(int, inp().split()))
+strl = lambda: list(inp().split())
+list_to_string = lambda _a: "".join(map(str, _a))
+list_to_string_list = lambda _a: " ".join(map(str, _a))
+_dp = lambda default_value: defaultdict(lambda: default_value)
+print_dp = lambda _dict: list(map(lambda item: print(f"{item[0]} = {item[1]}"), _dict.items()))
+list_from_inp = lambda n: [inp() for _ in range(n)]
+
+MOD = 10 ** 9 + 7
+INF = maxsize
+A = 911382323
+M = 9999999999879998
+
+
+# -------------------------------------------------------Solution-------------------------------------------------------
+
+def solve():
+    cost = inp()
+    return len(cost)
+
+
+def run():
+    t = iinp()
+
+    for _ in range(t):
+        print(solve())
+
+
+if __name__ == "__main__":
+    if os.environ.get("DEBUG_CODEFORCES"):
+        stdin = open("../input.txt", "r")
+        start_time = time.time()
+        run()
+        print("\n--- %s seconds ---\n" % (time.time() - start_time))
+    else:
+        run()
---
+++ Kattis/Python/1. easy/sanic.py
@@ -0,0 +1,60 @@
+# -----------------------------------------------------------
+# URL    : https://open.kattis.com/problems/sanic
+# Title  : Sanic
+# Notes  : tag-kattis, tag-easy
+# -----------------------------------------------------------
+
+# ---------------------------------------------------Shared part--------------------------------------------------------
+import math
+import os
+import time
+from collections import defaultdict, Counter
+from sys import stdin, maxsize, stdout
+
+inp = lambda: stdin.readline().strip()
+iinp = lambda: int(inp())
+intl = lambda: list(map(int, inp().split()))
+strl = lambda: list(inp().split())
+list_to_string = lambda _a: "".join(map(str, _a))
+list_to_string_list = lambda _a: " ".join(map(str, _a))
+_dp = lambda default_value: defaultdict(lambda: default_value)
+flush = lambda: stdout.flush()
+print_flush = lambda _text: (print(_text), flush())
+fact = lambda number: math.factorial(number)
+cnt = lambda _a: Counter(_a)
+
+
+def lcm(a, b):
+    return a * b // math.gcd(a, b)
+
+
+def print_dp(_dict):
+    for item in _dict.items():
+        print(f"{item[0]} = {item[1]}")
+
+
+MOD = 10 ** 9 + 7
+INF = maxsize
+A = 911382323
+M = 9999999999879998
+
+
+# -------------------------------------------------------Solution-------------------------------------------------------
+
+def solve():
+    r_big = float(inp())
+    return r_big - 1
+
+
+def run():
+    print(solve())
+
+
+if __name__ == "__main__":
+    if os.environ.get("DEBUG_CODEFORCES"):
+        stdin = open("../input.txt", "r")
+        start_time = time.time()
+        run()
+        print("\n--- %s seconds ---\n" % (time.time() - start_time))
+    else:
+        run()
