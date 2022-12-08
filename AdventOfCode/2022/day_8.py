7f388d7d159302d1049e49ad9fdb300ccd1b70e5
Valentinas <31697821+valentk777@users.noreply.github.com>
2022-12-08 09:30:23 +0200

    [AdventOfCode] Add few solutions

--- AdventOfCode/2022/day_8.py
+++ AdventOfCode/2022/day_8.py
@@ -1,7 +1,7 @@
 # ---------------------------------------------------------------------------------------
 # URL    : https://adventofcode.com/2022/day/8
-# Title  : PROBLEM_TITLE
-# Tags   : tag-adventofcode, tag-not-pass
+# Title  : Treetop Tree House
+# Tags   : tag-adventofcode
 # ---------------------------------------------------------------------------------------
 
 # region --------------------------------------------Shared part--------------------------------------------------------
@@ -9,7 +9,6 @@
 import math
 import sys
 from collections import defaultdict, Counter
-from typing import List
 
 from utils import read_lines
 
@@ -39,17 +38,151 @@ M = 9999999999879998
 
 # -------------------------------------------------------Solution-------------------------------------------------------
 
-def solve_1(data: List[str]) -> None:
-    pass
+def solve_1(data) -> None:
+    row = len(data)
+    col = len(data[0])
+    is_visible_matrix = [[False for _ in range(col)] for _ in range(row)]
+
+    for i in range(row):
+        for j in range(col):
+            if i == 0 or i == row - 1 or j == 0 or j == col - 1:
+                is_visible_matrix[i][j] = True
+
+    # from left
+    for i in range(1, row - 1):
+        _max = data[i][0]
+
+        for j in range(1, col - 1):
+            if _max < data[i][j]:
+                is_visible_matrix[i][j] = True
+
+            _max = max(_max, data[i][j])
+
+    # from right
+    for i in range(1, row - 1):
+        _max = data[i][-1]
+
+        for j in range(col - 2, 0, -1):
+            if _max < data[i][j]:
+                is_visible_matrix[i][j] = True
+
+            _max = max(_max, data[i][j])
+
+    # from top
+    for j in range(1, col - 1):
+        _max = data[0][j]
+
+        for i in range(1, row - 1):
+            if _max < data[i][j]:
+                is_visible_matrix[i][j] = True
+
+            _max = max(_max, data[i][j])
+
+    # from bottom
+    for j in range(1, col - 1):
+        _max = data[-1][j]
+
+        for i in range(row - 2, 0, -1):
+            if _max < data[i][j]:
+                is_visible_matrix[i][j] = True
+
+            _max = max(_max, data[i][j])
+
+    ans = sum(map(lambda x: sum(x), is_visible_matrix))
+    print(ans)
 
 
-def solve_2(data: List[str]) -> None:
-    pass
+def solve_2(data) -> None:
+    row = len(data)
+    col = len(data[0])
+    visible_distance_matrix = [[1 for _ in range(col)] for _ in range(row)]
+
+    # from left
+    for i in range(1, row - 1):
+        _max = data[i][0]
+        latest_view = [0 for _ in range(10)]
+
+        for j in range(1, col - 1):
+            if _max < data[i][j]:
+                visible_distance_matrix[i][j] *= j
+            else:
+                candidate = j
+
+                for k in range(data[i][j], 10):
+                    if latest_view[k] != 0:
+                        candidate = min(candidate, j - latest_view[k])
+
+                visible_distance_matrix[i][j] *= candidate
+
+            _max = max(_max, data[i][j])
+            latest_view[data[i][j]] = j
+
+    # from right
+    for i in range(1, row - 1):
+        _max = data[i][-1]
+        latest_view = [col for _ in range(10)]
+
+        for j in range(col - 2, 0, -1):
+            if _max < data[i][j]:
+                visible_distance_matrix[i][j] *= (col - j - 1)
+            else:
+                candidate = col - j - 1
+
+                for k in range(data[i][j], 10):
+                    if latest_view[k] != col:
+                        candidate = min(candidate, latest_view[k] - j)
+
+                visible_distance_matrix[i][j] *= candidate
+
+            _max = max(_max, data[i][j])
+            latest_view[data[i][j]] = j
+
+    # from top
+    for j in range(1, col - 1):
+        _max = data[0][j]
+        latest_view = [0 for _ in range(10)]
+
+        for i in range(1, row - 1):
+            if _max < data[i][j]:
+                visible_distance_matrix[i][j] *= i
+            else:
+                candidate = i
+
+                for k in range(data[i][j], 10):
+                    if latest_view[k] != 0:
+                        candidate = min(candidate, i - latest_view[k])
+
+                visible_distance_matrix[i][j] *= candidate
+
+            _max = max(_max, data[i][j])
+            latest_view[data[i][j]] = i
+
+    # from bottom
+    for j in range(1, col - 1):
+        _max = data[-1][j]
+        latest_view = [row for _ in range(10)]
+
+        for i in range(row - 2, 0, -1):
+            if _max < data[i][j]:
+                visible_distance_matrix[i][j] *= (row - i - 1)
+            else:
+                candidate = row - i - 1
+
+                for k in range(data[i][j], 10):
+                    if latest_view[k] != row:
+                        candidate = min(candidate, latest_view[k] - i)
+
+                visible_distance_matrix[i][j] *= candidate
+
+            _max = max(_max, data[i][j])
+            latest_view[data[i][j]] = i
+
+    print(max(map(lambda x: max(x), visible_distance_matrix)))
 
 
 if __name__ == "__main__":
     input_data = read_lines()
-    print(input_data)
+    input_data = list(map(lambda x: list(map(int, x)), input_data))
 
-    solve_1(input_data.copy())
-    solve_2(input_data.copy())
+    solve_1(input_data)
+    solve_2(input_data)
