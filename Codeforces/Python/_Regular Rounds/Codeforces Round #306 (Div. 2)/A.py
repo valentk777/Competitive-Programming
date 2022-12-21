249d73e4e86bf1a14f0ff2ef2d21271b84b53cf1
Valentinas <31697821+valentk777@users.noreply.github.com>
2022-12-21 09:14:40 +0200

    [Codeforces] Add few solutions

---
+++ Codeforces/Python/_Regular Rounds/Codeforces Round #306 (Div. 2)/A.py
@@ -0,0 +1,160 @@
+# ---------------------------------------------------------------------------------------
+# URL    : https://codeforces.com/contest/550/problem/A
+# Title  : Two Substrings
+# Tags   : tag-codeforces, tag-problem-A, tag-div-2, tag-difficulty-1500
+# Notes  : brute force, dp, greedy, implementation, strings
+# ---------------------------------------------------------------------------------------
+
+# region --------------------------------------------Shared part--------------------------------------------------------
+import math
+import os
+import sys
+import time
+from collections import defaultdict, Counter
+from io import IOBase, BytesIO
+
+inp = lambda: sys.stdin.readline().strip().rstrip("\r\n")
+iinp = lambda: int(inp())
+intl = lambda: list(map(int, inp().split()))
+strl = lambda: list(inp().split())
+list_to_string = lambda _a: "".join(map(str, _a))
+list_to_string_list = lambda _a: " ".join(map(str, _a))
+_dp = lambda default_value: defaultdict(lambda: default_value)
+flush = lambda: sys.stdout.flush()
+print_flush = lambda _text: (print(_text), flush())
+fact = lambda number: math.factorial(number)
+cnt = lambda _a: Counter(_a)
+
+
+def lcm(a, b):
+    return a * b // math.gcd(a, b)
+
+
+def print_dp(_dict):
+    for item in _dict.items():
+        print(f"{item[0]} = {item[1]}")
+
+
+def memodict(f):
+    """memoization decorator for a function taking a single argument"""
+
+    class memodict(dict):
+        def __missing__(self, key):
+            ret = self[key] = f(key)
+            return ret
+
+    return memodict().__getitem__
+
+
+MOD = 10 ** 9 + 7
+INF = sys.maxsize
+A = 911382323
+M = 9999999999879998
+
+# region -------------------------------------------Fast IO Region------------------------------------------------------
+BUFSIZE = 8192
+
+
+class FastIO(IOBase):
+    newlines = 0
+
+    def __init__(self, file):
+        self._fd = file.fileno()
+        self.buffer = BytesIO()
+        self.writable = "x" in file.mode or "r" not in file.mode
+        self.write = self.buffer.write if self.writable else None
+
+    def read(self):
+        while True:
+            b = os.read(self._fd, max(os.fstat(self._fd).st_size, BUFSIZE))
+            if not b:
+                break
+            ptr = self.buffer.tell()
+            self.buffer.seek(0, 2), self.buffer.write(b), self.buffer.seek(ptr)
+        self.newlines = 0
+        return self.buffer.read()
+
+    def readline(self):
+        while self.newlines == 0:
+            b = os.read(self._fd, max(os.fstat(self._fd).st_size, BUFSIZE))
+            self.newlines = b.count(b"\n") + (not b)
+            ptr = self.buffer.tell()
+            self.buffer.seek(0, 2), self.buffer.write(b), self.buffer.seek(ptr)
+        self.newlines -= 1
+        return self.buffer.readline()
+
+    def flush(self):
+        if self.writable:
+            os.write(self._fd, self.buffer.getvalue())
+            self.buffer.truncate(0), self.buffer.seek(0)
+
+
+class IOWrapper(IOBase):
+    def __init__(self, file):
+        self.buffer = FastIO(file)
+        self.flush = self.buffer.flush
+        self.writable = self.buffer.writable
+        self.write = lambda s: self.buffer.write(s.encode("ascii"))
+        self.read = lambda: self.buffer.read().decode("ascii")
+        self.readline = lambda: self.buffer.readline().decode("ascii")
+
+
+sys.stdin, sys.stdout = IOWrapper(sys.stdin), IOWrapper(sys.stdout)
+
+
+# endregion
+# endregion
+
+# -------------------------------------------------------Solution-------------------------------------------------------
+
+def solve():
+    s = inp()
+    n = len(s)
+
+    i = 1
+    found_ab = 0
+    found_ba = 0
+    both = 0
+    overlap_index = -1
+
+    while i < n:
+        if s[i - 1:i + 1] == "AB":
+            found_ab += 1
+
+            if overlap_index == i - 1:
+                both += 1
+
+            overlap_index = i
+        elif s[i - 1:i + 1] == "BA":
+            found_ba += 1
+
+            if overlap_index == i - 1:
+                both += 1
+
+            overlap_index = i
+        i += 1
+
+    if both > 2:
+        return "YES"
+
+    if found_ab - both > 0 and found_ba - both > 0:
+        return "YES"
+
+    if (found_ab - both > 0 or found_ba - both > 0) and both > 0:
+        return "YES"
+
+    return "NO"
+
+
+def run():
+    print(solve())
+
+
+if __name__ == "__main__":
+    if os.environ.get("DEBUG_CODEFORCES"):
+        sys.stdin = open("../../input.txt", "r")
+        start_time = time.time()
+        run()
+        print("\n--- %s seconds ---\n" % (time.time() - start_time))
+    else:
+        run()
