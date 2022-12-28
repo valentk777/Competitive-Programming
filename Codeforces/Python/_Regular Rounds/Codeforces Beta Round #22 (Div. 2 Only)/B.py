42d0135d294963bc0e6b5b886f2b55a0701b2555
Valentinas <31697821+valentk777@users.noreply.github.com>
2022-12-28 22:14:01 +0200

    [Codeforces] Add few solutions

---
+++ Codeforces/Python/_Regular Rounds/Codeforces Beta Round #22 (Div. 2 Only)/B.py
@@ -0,0 +1,140 @@
+# ---------------------------------------------------------------------------------------
+# URL    : https://codeforces.com/contest/22/problem/B
+# Title  : Bargaining Table
+# Tags   : tag-codeforces, tag-problem-B, tag-div-2, tag-difficulty-1500
+# Notes  : brute force, dp
+# ---------------------------------------------------------------------------------------
+
+# region --------------------------------------------Shared part--------------------------------------------------------
+import math
+import os
+import sys
+import time
+from collections import defaultdict, Counter
+from io import IOBase, BytesIO
+
+inp = lambda: sys.stdin.readline().strip().rstrip("\r\n")
+iinp = lambda: int(inp())
+intl = lambda: list(map(int, inp().split()))
+strl = lambda: list(inp().split())
+list_to_string = lambda _a: "".join(map(str, _a))
+list_to_string_list = lambda _a: " ".join(map(str, _a))
+_dp = lambda default_value: defaultdict(lambda: default_value)
+flush = lambda: sys.stdout.flush()
+print_flush = lambda _text: (print(_text), flush())
+fact = lambda number: math.factorial(number)
+cnt = lambda _a: Counter(_a)
+
+
+def lcm(a, b):
+    return a * b // math.gcd(a, b)
+
+
+def print_dp(_dict):
+    for item in _dict.items():
+        print(f"{item[0]} = {item[1]}")
+
+
+MOD = 10 ** 9 + 7
+INF = sys.maxsize
+A = 911382323
+M = 9999999999879998
+
+# region -------------------------------------------Fast IO Region------------------------------------------------------
+BUFSIZE = 8192
+
+
+class FastIO(IOBase):
+    newlines = 0
+
+    def __init__(self, file):
+        self._fd = file.fileno()
+        self.buffer = BytesIO()
+        self.writable = "x" in file.mode or "r" not in file.mode
+        self.write = self.buffer.write if self.writable else None
+
+    def read(self):
+        while True:
+            b = os.read(self._fd, max(os.fstat(self._fd).st_size, BUFSIZE))
+            if not b:
+                break
+            ptr = self.buffer.tell()
+            self.buffer.seek(0, 2), self.buffer.write(b), self.buffer.seek(ptr)
+        self.newlines = 0
+        return self.buffer.read()
+
+    def readline(self):
+        while self.newlines == 0:
+            b = os.read(self._fd, max(os.fstat(self._fd).st_size, BUFSIZE))
+            self.newlines = b.count(b"\n") + (not b)
+            ptr = self.buffer.tell()
+            self.buffer.seek(0, 2), self.buffer.write(b), self.buffer.seek(ptr)
+        self.newlines -= 1
+        return self.buffer.readline()
+
+    def flush(self):
+        if self.writable:
+            os.write(self._fd, self.buffer.getvalue())
+            self.buffer.truncate(0), self.buffer.seek(0)
+
+
+class IOWrapper(IOBase):
+    def __init__(self, file):
+        self.buffer = FastIO(file)
+        self.flush = self.buffer.flush
+        self.writable = self.buffer.writable
+        self.write = lambda s: self.buffer.write(s.encode("ascii"))
+        self.read = lambda: self.buffer.read().decode("ascii")
+        self.readline = lambda: self.buffer.readline().decode("ascii")
+
+
+sys.stdin, sys.stdout = IOWrapper(sys.stdin), IOWrapper(sys.stdout)
+
+
+# endregion
+# endregion
+
+# -------------------------------------------------------Solution-------------------------------------------------------
+
+def solve():
+    n, m = intl()
+    matrix = []
+
+    for _ in range(n):
+        matrix.append(list(map(int, list(inp()))))
+
+    ans = 0
+
+    for left_top in range(n):
+        for left_bottom in range(m):
+            for right_top in range(left_top, n):
+                for right_bottom in range(left_bottom, m):
+                    found_one = True
+
+                    for row in range(left_top, right_top + 1):
+                        if not found_one:
+                            break
+
+                        for column in range(left_bottom, right_bottom + 1):
+                            if matrix[row][column] == 1:
+                                found_one = False
+                                break
+
+                    if found_one:
+                        ans = max(ans, (right_top - left_top + 1 + right_bottom - left_bottom + 1) * 2)
+
+    return ans
+
+
+def run():
+    print(solve())
+
+
+if __name__ == "__main__":
+    if os.environ.get("DEBUG_CODEFORCES"):
+        sys.stdin = open("../../input.txt", "r")
+        start_time = time.time()
+        run()
+        print("\n--- %s seconds ---\n" % (time.time() - start_time))
+    else:
+        run()
