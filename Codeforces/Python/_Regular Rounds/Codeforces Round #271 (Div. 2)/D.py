f2d6ae5ec7cd928202be136077cc6d825c0b6722
Valentinas <31697821+valentk777@users.noreply.github.com>
2023-01-22 14:35:02 +0200

    [Codeforces] Add few solutions

---
+++ Codeforces/Python/_Regular Rounds/Codeforces Round #271 (Div. 2)/D.py
@@ -0,0 +1,150 @@
+# ---------------------------------------------------------------------------------------
+# URL    : https://codeforces.com/contest/474/problem/D
+# Title  : Flowers
+# Tags   : tag-codeforces, tag-problem-D, tag-div-2, tag-difficulty-1700
+# Notes  : dp
+# ---------------------------------------------------------------------------------------
+
+# region --------------------------------------------Shared part--------------------------------------------------------
+import math
+import os
+import sys
+import time
+from collections import defaultdict, Counter
+from io import IOBase, BytesIO
+
+inp = lambda: sys.stdin.readline().strip().rstrip("\r\n")
+iinp = lambda: int(inp())
+intl = lambda: list(map(int, inp().split()))
+strl = lambda: list(inp().split())
+list_to_string = lambda _a: "".join(map(str, _a))
+list_to_string_list = lambda _a: " ".join(map(str, _a))
+_dp = lambda default_value: defaultdict(lambda: default_value)
+flush = lambda: sys.stdout.flush()
+print_flush = lambda _text: (print(_text), flush())
+fact = lambda number: math.factorial(number)
+cnt = lambda _a: Counter(_a)
+
+
+def lcm(a, b):
+    return a * b // math.gcd(a, b)
+
+
+def print_dp(_dict):
+    for item in _dict.items():
+        print(f"{item[0]} = {item[1]}")
+
+
+def memodict(f):
+    """memoization decorator for a function taking a single argument"""
+
+    class memodict(dict):
+        def __missing__(self, key):
+            ret = self[key] = f(key)
+            return ret
+
+    return memodict().__getitem__
+
+
+MOD = 10 ** 9 + 7
+INF = sys.maxsize
+A = 911382323
+M = 9999999999879998
+yes = "YES"
+no = "NO"
+
+# region -------------------------------------------Fast IO Region------------------------------------------------------
+BUFSIZE = 8192
+
+
+class FastIO(IOBase):
+    newlines = 0
+
+    def __init__(self, file):
+        self._fd = file.fileno()
+        self.buffer = BytesIO()
+        self.writable = "x" in file.mode or "r" not in file.mode
+        self.write = self.buffer.write if self.writable else None
+
+    def read(self):
+        while True:
+            b = os.read(self._fd, max(os.fstat(self._fd).st_size, BUFSIZE))
+            if not b:
+                break
+            ptr = self.buffer.tell()
+            self.buffer.seek(0, 2), self.buffer.write(b), self.buffer.seek(ptr)
+        self.newlines = 0
+        return self.buffer.read()
+
+    def readline(self):
+        while self.newlines == 0:
+            b = os.read(self._fd, max(os.fstat(self._fd).st_size, BUFSIZE))
+            self.newlines = b.count(b"\n") + (not b)
+            ptr = self.buffer.tell()
+            self.buffer.seek(0, 2), self.buffer.write(b), self.buffer.seek(ptr)
+        self.newlines -= 1
+        return self.buffer.readline()
+
+    def flush(self):
+        if self.writable:
+            os.write(self._fd, self.buffer.getvalue())
+            self.buffer.truncate(0), self.buffer.seek(0)
+
+
+class IOWrapper(IOBase):
+    def __init__(self, file):
+        self.buffer = FastIO(file)
+        self.flush = self.buffer.flush
+        self.writable = self.buffer.writable
+        self.write = lambda s: self.buffer.write(s.encode("ascii"))
+        self.read = lambda: self.buffer.read().decode("ascii")
+        self.readline = lambda: self.buffer.readline().decode("ascii")
+
+
+sys.stdin, sys.stdout = IOWrapper(sys.stdin), IOWrapper(sys.stdout)
+
+
+# endregion
+# endregion
+
+# -------------------------------------------------------Solution-------------------------------------------------------
+
+def solve():
+    t, k = intl()
+
+    # number of combinations of length i, params: length of flowers in a dinner
+
+    dp = _dp(0)
+
+    for i in range(10 ** 5 + 1):
+        # when we increase number of possible places, but we cannot add any white one, so we add only one "R" * i
+        if i < k:
+            dp[i] = 1
+
+        # same amount of combinations as it was before if new character is R
+        # we have to have another k - 1 characters "W" before. we calculated this in i - k dp.
+        else:
+            dp[i] = (dp[i - 1] + dp[i - k]) % MOD
+
+    _sums = [1]
+
+    for i in range(1, 10 ** 5 + 1):
+        _sums.append((_sums[-1] + dp[i]) % MOD)
+
+    for _ in range(t):
+        a, b = intl()
+        print((_sums[b] - _sums[a - 1]) % MOD)
+
+
+def run():
+    solve()
+
+
+if __name__ == "__main__":
+    if os.environ.get("DEBUG_CODEFORCES"):
+        sys.stdin = open("../../input.txt", "r")
+        start_time = time.time()
+        run()
+        print("\n--- %s seconds ---\n" % (time.time() - start_time))
+    else:
+        run()
